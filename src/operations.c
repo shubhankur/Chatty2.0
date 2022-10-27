d138267e780e22ca1d1a57afc271a34a00d831fd
Shubhankar Kumar <shubhankur99@gmail.com>
2022-10-26 23:14:20 -0400

    WORKING

--- src/operations.c
+++ src/operations.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <string.h>
-
 #include <unistd.h>
-
 #include <sys/types.h>
-
 #include <sys/socket.h>
-
 #include <sys/stat.h>
-
 #include <netinet/in.h>
-
 #include <arpa/inet.h>
-
 #include <netdb.h>
-
 #include <stdbool.h>
-
 #include "../include/global.h"
 #include "../include/logger.h"
 #include "../include/universalMethods.h"
