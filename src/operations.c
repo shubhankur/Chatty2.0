b903839730923545c2bf9f11d662020cfe1b34cb
Shubhankar Kumar <shubhankur99@gmail.com>
2022-10-26 23:34:53 -0400

    Put structs in a header file

--- src/operations.c
+++ src/operations.c
@@ -13,32 +13,11 @@
 #include "../include/logger.h"
 #include "../include/universalMethods.h"
 #include "../include/operations.h"
+#include "../include/structs.h"
 
 #define dataSizeMax 500
 #define dataSizeMaxBg 500 * 200
 #define STDIN 0
-
-struct message {
-    char text[500];
-    struct host * from_client;
-    struct message * next_message;
-    bool is_broadcast;
-};
-
-struct host {
-    char hostname[500];
-    char ip[500];
-    char port[500];
-    char status[500];
-    int sentMsgCount;
-    int recvMsgCount;
-    int fd;
-    struct host * blocked;
-    struct host * next_host;
-    bool loggedIn;
-    bool checkServer;
-    struct message * queued_messages;
-};
 
 // INITIALISE GLOBAL VARIABLES
 struct host * clientNew = NULL; //to store new clients
